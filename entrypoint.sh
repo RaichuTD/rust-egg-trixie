8b4e80790ac80d4f55da3c8a8ed397d3a700b66a
Raichu <32996424+RaichuTD@users.noreply.github.com>
2026-05-24 20:57:12 +0100

    Clean up entrypoint.sh by removing debug echoes
    Removed debugging output for environment variables.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 cd /home/container
 
-# Display environment variables for debugging
-echo "Starting container with the following environment:"
-echo "Server Port: ${SERVER_PORT}"
-
-# Replace Pterodactyl startup variable
 MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo "Starting server with command: ${MODIFIED_STARTUP}"
-
-# Run the startup command
-eval "${MODIFIED_STARTUP}"
+eval ${MODIFIED_STARTUP}
